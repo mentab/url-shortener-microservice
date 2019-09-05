0b22a6e1a0d07f1ee9841ffcbb3375fb50ba123c
Glitch (cleabr-url-shortener-microservice) <none>
2019-09-05 11:33:48 +0000

    🎮🦀 Updated with Glitch

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-'use strict';
-
 var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
-var cors = require('cors');
-
 var app = express();
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var router = express.Router();
 
-// Basic Configuration 
 var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+var findOneByShortUrl = require('./shortUrl.js').findOneByShortUrl;
+app.get("/api/shorturl/:shortUrl", function (req, res, next) {
+  findOneByShortUrl(req.params.shortUrl, function(err, data) {
+      if(err) res.json(err);
+      if(!data) res.json({message: 'Missing callback argument'});
+      res.redirect(data.originalUrl);
+  });
+});
+
+var createAndSaveUrl = require('./shortUrl.js').createAndSaveUrl;
+app.post("/api/shorturl/new", function (req, res, next) {
+  createAndSaveUrl(req.body.url, function(err, data) {
+    if(err) res.json(err);
+    if(!data) res.json({message: 'Missing callback argument'});
+    res.json({original_url: data.originalUrl, short_url: data.shortUrl});
+  });
 });
 
 app.listen(port, function () {
